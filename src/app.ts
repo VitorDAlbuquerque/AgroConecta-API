083324af722d1bbb3994ae376a27994e188b6015
Kauan Santos <85942974+Kauan-afk@users.noreply.github.com>
2025-09-10 10:08:47 -0300

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application } from 'express'
+import { router } from './routes';
+import cors from 'cors'
+import { resolve } from 'path'
+
+const app: Application = express()
+
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.static(resolve('uploads')))
+app.use(router)
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello World' })
+})
+
+export { app };
